020a7cf11bbc88dabd4e640102b848afea278c4d
lvcasbecker <lvcasb3cker@gmail.com>
2026-08-23 22:12:11 -0300

    Update site.js

--- site.js
+++ site.js
@@ -19,17 +19,23 @@ function nomeDaCategoria(slug) {
 }
 
 function renderMidia(m, altBase) {
+  // largura de cada mídia: "dupla" (2 por fileira, padrão),
+  // "trio" (3 por fileira) ou "sozinha" (1 na fileira)
+  let cl = '';
+  if (m.largura === 'trio') cl = ' class="m-trio"';
+  if (m.largura === 'sozinha') cl = ' class="m-sozinha"';
+  const clEmbed = m.largura === 'trio' ? ' m-trio' : (m.largura === 'sozinha' ? ' m-sozinha' : '');
   if (m.tipo === 'imagem') {
-    return `<img src="${m.src}" alt="${altBase}" loading="lazy">`;
+    return `<img${cl} src="${m.src}" alt="${altBase}" loading="lazy">`;
   }
   if (m.tipo === 'video') {
-    return `<video src="${m.src}" controls playsinline preload="metadata"></video>`;
+    return `<video${cl} src="${m.src}" controls playsinline preload="metadata"></video>`;
   }
   if (m.tipo === 'youtube') {
-    return `<div class="midia-embed"><iframe src="https://www.youtube-nocookie.com/embed/${m.id}" title="${altBase}" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen loading="lazy"></iframe></div>`;
+    return `<div class="midia-embed${clEmbed}"><iframe src="https://www.youtube-nocookie.com/embed/${m.id}" title="${altBase}" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen loading="lazy"></iframe></div>`;
   }
   if (m.tipo === 'vimeo') {
-    return `<div class="midia-embed"><iframe src="https://player.vimeo.com/video/${m.id}?dnt=1" title="${altBase}" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen loading="lazy"></iframe></div>`;
+    return `<div class="midia-embed${clEmbed}"><iframe src="https://player.vimeo.com/video/${m.id}?dnt=1" title="${altBase}" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen loading="lazy"></iframe></div>`;
   }
   return '';
 }
